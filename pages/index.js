f7e9b37b1b0572ed94bcab63929392fa975e51cf
kene3055 <koffokansi@gmail.com>
2022-02-23 17:55:54 +0100

    looking for heroku server name

--- pages/index.js
+++ pages/index.js
@@ -55,10 +55,10 @@ export default function Home({
 export async function getServerSideProps() {
     try {
         const userResponse = await fetch(
-            "https://git.heroku.com/edvora30555.com/api/user-info"
+            "https://edvora30555.herokuapp.com//api/user-info"
         );
         const rideResponse = await fetch(
-            "https://git.heroku.com/edvora30555.com/api/rides"
+            "https://edvora30555.herokuapp.com//api/rides"
         );
 
         if (userResponse.status >= 300 || rideResponse.status >= 300) {
